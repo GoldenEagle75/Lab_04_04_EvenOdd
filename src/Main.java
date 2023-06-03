4651573017958967b0bb22f8c42d7bc9d6b5d311
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-06-03 13:31:09 -0400

    Finished Project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 2; //how am I supposed to compare these without an if statement?
+        if (numToExamine%2 == 0){
+            System.out.println(numToExamine + " is an even number.");
+        }
+        else {
+            System.out.println(numToExamine + " is an odd number.");
+        }
+    }
+}
